f456dca61f282b0c2029aecb67a45679cf3c13a1
lancelotluo <chengzju@qq.com>
2019-06-17 20:05:39 +0800

    success to compile showquic.go

---
+++ showquic.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "reflect"
+    "strings"
+
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/layers"
+    "github.com/google/gopacket/pcap"
+)
+
+func main() {
+    path := "/Users/liruopeng/Downloads/test2.pcap"
+    handler, err := pcap.OpenOffline(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer handler.Close()
+
+    packetSource := gopacket.NewPacketSource(handler, handler.LinkType())
+
+    i := 0
+    for packet := range packetSource.Packets() {
+        // if i < 1 {
+        //  fmt.Println(packet)
+        //  // break
+        // }
+        i++
+        if i == 2915 {
+            printPacketInfo(packet)
+        }
+    }
+
+    fmt.Println(i)
+}
+
+func printPacketInfo(packet gopacket.Packet) {
+    // Let's see if the packet is an ethernet packet
+    ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+    if ethernetLayer != nil {
+        fmt.Println("Ethernet layer detected.")
+        ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+        fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+        fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+        // Ethernet type is typically IPv4 but could be ARP or other
+        fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+        fmt.Println()
+    }
+
+    // Let's see if the packet is IP (even though the ether type told us)
+    ipLayer := packet.Layer(layers.LayerTypeIPv4)
+    if ipLayer != nil {
+        fmt.Println("IPv4 layer detected.")
+        ip, _ := ipLayer.(*layers.IPv4)
+
+        // IP layer variables:
+        // Version (Either 4 or 6)
+        // IHL (IP Header Length in 32-bit words)
+        // TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+        // Checksum, SrcIP, DstIP
+        fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+        fmt.Println("Protocol: ", ip.Protocol)
+        fmt.Println()
+    }
+
+    // Let's see if the packet is TCP
+    tcpLayer := packet.Layer(layers.LayerTypeTCP)
+    if tcpLayer != nil {
+        fmt.Println("TCP layer detected.")
+        tcp, some := tcpLayer.(*layers.TCP)
+        // tcp := tcpLayer
+        fmt.Println(reflect.TypeOf(tcp), reflect.TypeOf(tcpLayer))
+        // fmt.Println(tcpLayer)
+        fmt.Println("some=", some)
+
+        // TCP layer variables:
+        // SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+        // Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+        // fmt.Printf("From port %d to %d\n", tcpLayer.SrcPort, tcpLayer.DstPort)
+        fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+        fmt.Println("Sequence number: ", tcp.Seq)
+        fmt.Println()
+    }
+
+    // Iterate over all layers, printing out each layer type
+    fmt.Println("All packet layers:")
+    for _, layer := range packet.Layers() {
+        fmt.Println("- ", layer.LayerType())
+    }
+
+    // When iterating through packet.Layers() above,
+    // if it lists Payload layer then that is the same as
+    // this applicationLayer. applicationLayer contains the payload
+    applicationLayer := packet.ApplicationLayer()
+    if applicationLayer != nil {
+        fmt.Println("Application layer/Payload found.")
+        fmt.Printf("%s\n", applicationLayer.Payload())
+
+        // Search for a string inside the payload
+        if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+            fmt.Println("HTTP found!")
+        }
+    }
+
+    // Check for errors
+    if err := packet.ErrorLayer(); err != nil {
+        fmt.Println("Error decoding some part of the packet:", err)
+    }
+}
